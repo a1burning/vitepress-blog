0e01e31d7442c8eb1485b544c22014df5212f537
chengshuangfeng <chengshuangfeng@kkworld.com>
2024-06-18 00:04:33 +0800

    feat: 修改发布文件

--- shell/deploy-csf.sh
+++ shell/deploy-csf.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # 在这里编写项目的提测脚本
 #!/bin/sh
-rm -rf docs/.vuepress/dist
-dns=audition
-name=vuepress-$dns
+rm -rf docs/.vitepress/dist
+dns=blog
+name=vitepress-$dns
 # 打包
 echo building project $name:
-npm run docs:build
+npm run build
 # 发布
 echo send project $name:
-rsync -arz docs/.vuepress/dist/ root@www.hu77.top:/xp/www/home/$name/
+rsync -arz docs/.vitepress/dist/ root@www.hu77.top:/xp/www/home/$name/
 
 echo you can visit http://$dns.hu77.top/
